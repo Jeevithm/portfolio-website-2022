574a8931af54ee43f4f97dcafcb39d9d7f3e20bb
Jeevith <jeevithm@yahoo.dk>
2022-02-22 14:23:56 +0100

    Updating work so far

--- assets/js/main.js
+++ assets/js/main.js
@@ -135,18 +135,20 @@ sr.reveal('.home__social, .home__scroll', {delay: 900, origin: 'bottom'})
 
 /*=============== Read more Read Less Button ===============*/
 /* not using */
-function myFunction() {
-    var dots = document.getElementById("dots");
-    var moreText = document.getElementById("more");
-    var btnText = document.getElementById("myBtn");
-  
-    if (dots.style.display === "none") {
-      dots.style.display = "inline";
-      btnText.innerHTML = "Read more"; 
-      moreText.style.display = "none";
-    } else {
-      dots.style.display = "none";
-      btnText.innerHTML = "Read less"; 
-      moreText.style.display = "inline";
-    }
-  }
+const parentContainer =  document.querySelector('.testimonial__container');
+
+parentContainer.addEventListener('click', event=>{
+
+    const current = event.target;
+
+    const isReadMoreBtn = current.className.includes('read-more-btn');
+
+    if(!isReadMoreBtn) return;
+
+    const currentText = event.target.parentNode.querySelector('.read-more-text');
+
+    currentText.classList.toggle('read-more-text--show');
+
+    current.textContent = current.textContent.includes('Read More') ? "Read Less..." : "Read More...";
+
+})
